11e756fa308192a883b6b654eb3df53e99ba643a
YHI <fireyang@gmail.com>
2025-08-13 10:43:52 +0900

    51로 수정

--- src/main/java/site/metacoding/awsv5/web/BookApiController.java
+++ src/main/java/site/metacoding/awsv5/web/BookApiController.java
@@ -22,7 +22,7 @@ public class BookApiController {
 
     @GetMapping("/")
     public String home() {
-        return "<h1>aws-v5</h1>";
+        return "<h1>aws-v51</h1>";
     }
 
     @PostMapping("/api/book")
